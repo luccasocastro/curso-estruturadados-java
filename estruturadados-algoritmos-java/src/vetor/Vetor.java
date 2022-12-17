3862432aa660c0f40b8b7745dcb207c691ceed81
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-12-16 23:47:35 -0300

    criando método adiciona do vetor

--- estruturadados-algoritmos-java/src/vetor/Vetor.java
+++ estruturadados-algoritmos-java/src/vetor/Vetor.java
@@ -3,8 +3,31 @@ package vetor;
 public class Vetor {
     
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade){
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    /* 
+    public void adiciona(String elemento){
+        for(int i=0; i<this.elementos.length; i++){
+            if(this.elementos[i] == null){
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
+    }
+    */
+
+    public Boolean adiciona(String elemento){
+
+        if(tamanho < this.elementos.length){
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
     }
 }
