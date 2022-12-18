7f070a7d918d40c279d5999c89a760c11ca3bac3
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-12-18 18:59:33 -0300

    removendo elemento do vetor

---
+++ estruturadados-algoritmos-java/src/vetor/testes/Aula09.java
@@ -0,0 +1,22 @@
+package vetor.testes;
+
+import vetor.Vetor;
+
+public class Aula09 {
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(3);
+
+        vetor.adiciona("B");
+        vetor.adiciona("C");
+        vetor.adiciona("E");
+        vetor.adiciona("F");
+        vetor.adiciona("G");
+
+        System.out.println(vetor);
+
+        vetor.remove(1);
+
+        System.out.println(vetor);
+    }
+}
