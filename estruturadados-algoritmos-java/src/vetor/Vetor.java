ac84783892465b0b84001fc5ada174514e23bb7e
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-12-18 18:16:31 -0300

    adicionando elemento em qualquer posicao

--- estruturadados-algoritmos-java/src/vetor/Vetor.java
+++ estruturadados-algoritmos-java/src/vetor/Vetor.java
@@ -31,6 +31,20 @@ public class Vetor {
         return false;
     }
 
+    public Boolean adiciona(int posicao, String elemento){
+        if(!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        for(int i=this.tamanho-1; i>=posicao; i--){
+            this.elementos[i+1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return false;
+    }
+
     public String busca(int posicao){
         if(!(posicao >= 0 && posicao < tamanho)){
             throw new IllegalArgumentException("Posição inválida");
